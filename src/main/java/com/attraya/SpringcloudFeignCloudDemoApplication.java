057677d47869c1433ac7cabc48333b01e5462334
Attraya Das <attrayaghoshdas@gmail.com>
2024-01-04 00:05:07 +0530

    Add REST API to find by id

--- src/main/java/com/attraya/SpringcloudFeignCloudDemoApplication.java
+++ src/main/java/com/attraya/SpringcloudFeignCloudDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +23,11 @@ public class SpringcloudFeignCloudDemoApplication {
 	@GetMapping("/findAllUsers")
 	public List<User> getAllUsers(){
 		return userClient.getUsers();
+	}
+
+	@GetMapping("findById/{id}")
+	public User getUserById(@PathVariable int id){
+		return userClient.getUsersById(id);
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/attraya/client/UserClient.java
+++ src/main/java/com/attraya/client/UserClient.java
@@ -3,11 +3,16 @@ package com.attraya.client;
 import com.attraya.dto.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 
 @FeignClient(url = "https://jsonplaceholder.typicode.com", name = "USER-CLIENT")
 public interface UserClient {
 
     @GetMapping("/users")
-    public List<User> getUsers();
+    List<User> getUsers();
+
+    @GetMapping("/users/{id}")
+    User getUsersById(@PathVariable int id);
 }
